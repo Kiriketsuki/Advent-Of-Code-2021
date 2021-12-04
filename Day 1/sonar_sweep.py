68ad277c8ad470a19e7fac6599d40aa530f4d7c8
Jovian Lim <kiriketsuki@gmail.com>
2021-12-04 23:38:04 +0800

    day 1 complete

--- Day 1/sonar_sweep.py
+++ Day 1/sonar_sweep.py
@@ -1,5 +1,6 @@
 # Long list of number. Iterate through and check if curr > prev. Increase count
 
+# * part 1
 import os
 
 file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
@@ -10,14 +11,28 @@ with open(file_path) as f:
     for i, line in enumerate(lines):
         lines[i] = line.strip()
 
+def count_bigger(arr):
+    counter = -1 # to deal with the first input not having a greater one
+    prev = float('-inf')
 
-counter = -1 # to deal with the first input not having a greater one
-prev = float('-inf')
+    for i in arr:
+        curr = float(i)
+        if curr > prev:
+            counter += 1
+        prev = curr
 
-for i in lines:
-    curr = float(i)
-    if curr > prev:
-        counter += 1
-    prev = curr
+    return counter
 
-print(counter)
+print(count_bigger(lines))
+
+# * part 2
+
+to_subtract = 0
+new_array = []
+
+for i in range(2, len(lines)): 
+    sum = int(lines[i-2]) + int(lines[i-1]) + int(lines[i])
+    new_array.append(sum)
+
+print(count_bigger(new_array))
+
